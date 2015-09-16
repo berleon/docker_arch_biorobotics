ee9a6dc26111361a89e1ae3bc52bb39b4db64c3a
berleon <github@leon-sixt.de>
2015-09-16 18:59:08 +0200

    parallise caffe build

--- build_caffe.sh
+++ build_caffe.sh
@@ -2,5 +2,5 @@ git clone https://github.com/BVLC/caffe.git /opt/caffe
 mkdir -pv /opt/caffe/build
 cd /opt/caffe/build
 cmake -DCMAKE_CXX_COMPILER=/usr/bin/c++ -DBLAS=open -DCMAKE_BUILD_TYPE=Release ..
-make all
+make all -j `nprog`
 make install
